7c3a5d5a4f1aa5a29909171cf4097875572bf4c9
ItsTheWolf <danielwolf194@gmail.com>
2018-05-19 19:25:05 +0700

    remove comments

--- src/main/java/com/mycompany/banhangonline/dao/RoleDAO.java
+++ src/main/java/com/mycompany/banhangonline/dao/RoleDAO.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.banhangonline.dao;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-/**
- *
- * @author danie
- */
 public class RoleDAO {
 
     private EntityManager em;
