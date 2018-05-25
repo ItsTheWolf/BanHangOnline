d5fe42dfd088aa694e5238c6292ed65c8686a2c6
ItsTheWolf <danielwolf194@gmail.com>
2018-05-25 16:34:09 +0700

    fix useredit and usereditpw error doesn't return to correct page + add new test case

--- src/main/java/com/mycompany/showroomonline/bus/UserServlets/UserEditPWServlet.java
+++ src/main/java/com/mycompany/showroomonline/bus/UserServlets/UserEditPWServlet.java
@@ -3,6 +3,7 @@ package com.mycompany.showroomonline.bus.UserServlets;
 import com.mycompany.showroomonline.dao.UserDAO;
 import com.mycompany.showroomonline.dto.User;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
@@ -19,7 +20,7 @@ public class UserEditPWServlet extends HttpServlet {
     private UserDAO userDAO = new UserDAO();
     String REQUIRED_FIELDS_BLANK = "Please fill in the required (*) fields.";
     String PASSWORD_CPW_NOT_MATHCED = "Password and Confirm Password do not match.";
-    String BACK1 = "Click <a href='/usereditpw?username=";
+    String BACK1 = "Click <a href='usereditpw?username=";
     String BACK2 = "'>here</a> to turn back.";
 
     @Override
@@ -51,12 +52,13 @@ public class UserEditPWServlet extends HttpServlet {
             session.setAttribute("BACK", BACK1 + username + BACK2);
             String password = request.getParameter("txtPassword");
             String cpw = request.getParameter("txtCPassword");
-            boolean error = validation(password, cpw, session);
+            boolean error = validation(password, cpw, session, request);
             if (error) {
                 response.sendRedirect(request.getContextPath() + "/error.jsp");
             } else {
-                User item = new User(username, password);
-                userDAO.updateUser(item);
+                User user = userDAO.read(username);
+                user.setPassword(password);
+                userDAO.updateUser(user);
                 response.sendRedirect(request.getContextPath() + "/userindex");
             }
         } catch (Exception e) {
@@ -64,13 +66,19 @@ public class UserEditPWServlet extends HttpServlet {
         }
     }
 
-    protected boolean validation(String pw, String cpw, HttpSession session) {
+    protected boolean validation(String pw, String cpw, HttpSession session, HttpServletRequest request) throws UnsupportedEncodingException {
+        request.setCharacterEncoding("UTF-8");
+        int err1 = 0, err2 = 0;
         try {
-            if (!cpw.equals(pw)) {
-                session.setAttribute("ERROR1", PASSWORD_CPW_NOT_MATHCED);
-                return true;
+            if (pw.equals("") || cpw.equals("")) {
+                session.setAttribute("ERROR1", REQUIRED_FIELDS_BLANK);
+                err1 = 1;
             }
-            return false;
+            if (!cpw.equals(pw)) {
+                session.setAttribute("ERROR2", PASSWORD_CPW_NOT_MATHCED);
+                err2 = 1;
+            }
+            return err1 == 1 || err2 == 1;
         } catch (Exception e) {
             Logger.getLogger(UserCreateServlet.class.getName()).log(Level.SEVERE, null, e);
             return true;
